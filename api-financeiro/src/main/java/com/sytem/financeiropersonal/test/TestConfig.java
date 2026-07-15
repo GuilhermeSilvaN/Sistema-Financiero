adc2adf01102e08cb906db2bd0a0abb126839444
GuilhermeSilvaN <guilhermesilvaalvescardoso@gmail.com>
2026-07-15 14:09:06 -0300

    test

--- api-financeiro/src/main/java/com/sytem/financeiropersonal/test/TestConfig.java
+++ api-financeiro/src/main/java/com/sytem/financeiropersonal/test/TestConfig.java
@@ -1,13 +1,7 @@
 package com.sytem.financeiropersonal.test;
 
-import com.sytem.financeiropersonal.model.Dashboard;
-import com.sytem.financeiropersonal.model.Despesa;
-import com.sytem.financeiropersonal.model.Entrada;
-import com.sytem.financeiropersonal.model.UserEntity;
-import com.sytem.financeiropersonal.repository.DashboardRepository;
-import com.sytem.financeiropersonal.repository.DespesaRepository;
-import com.sytem.financeiropersonal.repository.EntradaRepository;
-import com.sytem.financeiropersonal.repository.UserEntityRepository;
+import com.sytem.financeiropersonal.model.*;
+import com.sytem.financeiropersonal.repository.*;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -18,88 +12,42 @@ import java.time.LocalDate;
 @Profile("dev")
 public class TestConfig implements CommandLineRunner {
 
-    private final UserEntityRepository userEntityRepository;
-    private final EntradaRepository  entradaRepository;
-    private final DespesaRepository despesaRepository;
-    private final DashboardRepository dashboardRepository;
 
     public TestConfig(
-            UserEntityRepository userEntityRepository,
-            DashboardRepository dashboardRepository,
-            EntradaRepository entradaRepository,
-            DespesaRepository despesaRepository
     ) {
-        this.userEntityRepository = userEntityRepository;
-        this.dashboardRepository = dashboardRepository;
-        this.entradaRepository = entradaRepository;
-        this.despesaRepository = despesaRepository;
+
     }
 
     @Override
     public void run(String... args) throws Exception {
-
-        entradaRepository.deleteAll();
-        despesaRepository.deleteAll();
-        dashboardRepository.deleteAll();
-
-        UserEntity user = userEntityRepository.findByEmailAndIsActiveTrue("isagi@gmail.com", true);
-
-
-        // DASHBOARD
-        Dashboard dashboard_1 = new Dashboard(user);
-
-        dashboardRepository.save(dashboard_1);
-
-        // ENTRADAS
-        Entrada entrada1 = new Entrada(
-                LocalDate.of(2026, 5, 5),
-                "Salário",
-                3500.0
-        );
-        entrada1.setDashboard(dashboard_1);
-
-        Entrada entrada2 = new Entrada(
-                LocalDate.of(2026, 5, 15),
-                "venda",
-                120.00
-        );
-        entrada2.setDashboard(dashboard_1);
-
-        dashboard_1.getEntradas().add(entrada1);
-        dashboard_1.getEntradas().add(entrada2);
-
-        // DESPESAS
-        Despesa despesa1 = new Despesa(
-                LocalDate.of(2026, 5, 15),
-                "Aluguel",
-                "Moradia",
-                "PIX",
-                1200.0
-        );
-        despesa1.setDashboard(dashboard_1);
-
-        Despesa despesa2 = new Despesa(
-                LocalDate.of(2026, 5, 10),
-                "Internet",
-                "Serviços",
-                "Cartão",
-                139.0
-        );
-        despesa2.setDashboard(dashboard_1);
-
-        dashboard_1.getDespesas().add(despesa1);
-        dashboard_1.getDespesas().add(despesa2);
-
-        entradaRepository.save(entrada1);
-        entradaRepository.save(entrada2);
-
-        despesaRepository.save(despesa1);
-        despesaRepository.save(despesa2);
-
-        dashboardRepository.save(dashboard_1);
-
-        System.out.println("Banco populado com sucesso!");
-
-
+//        entradaRepository.deleteAll();
+//        despesaRepository.deleteAll();
+//        mesDashboardRepository.deleteAll();
+//        dashboardRepository.deleteAll();
+//
+//        UserEntity user1 = userEntityRepository
+//                .findByEmailAndIsActiveTrue("isagi@gmail.com", true);
+//
+//        Dashboard dashboard1 = new Dashboard();
+//
+//        dashboard1.addEntrada(
+//                new Entrada(
+//                        LocalDate.of(2026, 7, 5),
+//                        "salario",
+//                        1400.00)
+//        );
+//        dashboard1.addDespesa(
+//                new Despesa(
+//                       LocalDate.of(2026, 7, 6),
+//                       "chocolates",
+//                       "besteiras",
+//                       "PIX",
+//                       7.99
+//                )
+//        );
+//
+//        MesDashboard mesDashboard1 = new MesDashboard(7, 2026, user1,  dashboard1);
+//        mesDashboardRepository.save(mesDashboard1);
     }
+
 }
